4fdb722a6b3a1aeb8cc12c3f54005c4e804bc988
Hieu Nguyen <ngohoanglongptit@gmail.com>
2023-01-30 14:25:11 +0700

    update

--- index.js
+++ index.js
@@ -30,33 +30,47 @@ var download = async function (url, dest, cb) {
   let arr = await fetch(sourceUrl).then((res) =>
     res.text().then((text) => text.split("\n"))
   );
+  let end = false;
   let index = 0;
-  while (index < arr.length - 1) {
-    const url = arr[index];
-    if (!url.includes("###")) {
-      var filename = url.substring(url.lastIndexOf("/") + 1);
-      var dir =
-        "./images" +
-        url
-          .replace("https://static.mobelaris.com", "")
-          .replace(filename, "")
-          .trim();
-      const dest = dir + "/" + filename;
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-      }
-      try {
-        console.log("start", { url, dest });
-        await new Promise((resolve, reject) => {
-          download(url, dest, () => {
-            console.log("end", { url, dest });
-            resolve();
-          });
+  while (!end) {
+    try {
+      await new Promise((resolve, reject) => {
+        new Array(10).fill(true).forEach(async (item, i, list) => {
+          if (index === arr.length) {
+            console.log(index, arr.length);
+            end = true;
+            return;
+          }
+          const url = arr[index];
+          index++;
+          if (!url.includes("###")) {
+            var filename = url.substring(url.lastIndexOf("/") + 1);
+            var dir =
+              "./images" +
+              url
+                .replace("https://static.mobelaris.com", "")
+                .replace(filename, "")
+                .trim();
+            const dest = dir + "/" + filename;
+            if (!fs.existsSync(dir)) {
+              fs.mkdirSync(dir, { recursive: true });
+            }
+            try {
+              console.log("start", { url, dest });
+              download(url, dest, () => {
+                console.log("end", { url, dest });
+              });
+              if (i === list.length - 1) {
+                resolve();
+              }
+            } catch (error) {
+              console.error(error, url);
+            }
+          }
         });
-      } catch (error) {
-        console.error(error, url);
-      }
+      });
+    } catch (error) {
+      console.error(error);
     }
-    index++;
   }
 })();
